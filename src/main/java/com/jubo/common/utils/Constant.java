339598c97decb5edacf0d85c689806410d74688b
pengxiao <pengxiao@weshare.com.cn>
2017-07-22 22:23:28 +0800

    全局变量

--- src/main/java/com/jubo/common/utils/Constant.java
+++ src/main/java/com/jubo/common/utils/Constant.java
@@ -2,27 +2,40 @@ package com.jubo.common.utils;
 
 /**
  * 常量
- * 
+ *
  * @author chenshun
  * @email sunlightcs@gmail.com
  * @date 2016年11月15日 下午1:23:52
  */
 public class Constant {
-	/** 超级管理员ID */
-	public static final int SUPER_ADMIN = 1;
+    /**
+     * 超级管理员ID
+     */
+    public static final int SUPER_ADMIN = 1;
 
-	/**
-	 * 菜单类型
-	 * 
-	 * @author chenshun
-	 * @email sunlightcs@gmail.com
-	 * @date 2016年11月15日 下午1:24:29
-	 */
+
+    /**
+     * 生效
+     **/
+    public static final int ENABLE = 1;
+
+    /**
+     * 失效
+     **/
+    public static final int DISABLE = 0;
+
+    /**
+     * 菜单类型
+     *
+     * @author chenshun
+     * @email sunlightcs@gmail.com
+     * @date 2016年11月15日 下午1:24:29
+     */
     public enum MenuType {
         /**
          * 目录
          */
-    	CATALOG(0),
+        CATALOG(0),
         /**
          * 菜单
          */
@@ -42,10 +55,10 @@ public class Constant {
             return value;
         }
     }
-    
+
     /**
      * 定时任务状态
-     * 
+     *
      * @author chenshun
      * @email sunlightcs@gmail.com
      * @date 2016年12月3日 上午12:07:22
@@ -54,18 +67,18 @@ public class Constant {
         /**
          * 正常
          */
-    	NORMAL(0),
+        NORMAL(0),
         /**
          * 暂停
          */
-    	PAUSE(1);
+        PAUSE(1);
 
         private int value;
 
         private ScheduleStatus(int value) {
             this.value = value;
         }
-        
+
         public int getValue() {
             return value;
         }
--- src/main/resources/static/js/modules/sys/goods.js
+++ src/main/resources/static/js/modules/sys/goods.js
@@ -2,121 +2,129 @@ $(function () {
     $("#jqGrid").jqGrid({
         url: baseURL + 'goods/list',
         datatype: "json",
-        colModel: [			
-			{ label: 'id', name: 'id', index: 'id', width: 50, key: true },
-			{ label: '商品名称', name: 'name', index: 'name', width: 80 }, 			
-			{ label: '1设备使用次数（单位：次）2账户充值（单位：元）', name: 'type', index: 'type', width: 80 }, 			
-			{ label: '商品值', name: 'value', index: 'value', width: 80 }, 			
-			{ label: '商品价格', name: 'money', index: 'money', width: 80 }, 			
-			{ label: '', name: 'createTime', index: 'create_time', width: 80 }, 			
-			{ label: '状态 1生效 0失效', name: 'status', index: 'status', width: 80 }			
+        colModel: [
+            {label: 'id', name: 'id', index: 'id', width: 50, key: true, hidden: true},
+            {label: '商品名称', name: 'name', index: 'name', width: 80},
+            {
+                label: '商品类型', name: 'type', index: 'type', width: 80, formatter: function (value, options, row) {
+                if (value === 1) {
+                    return '<span">设备使用次数</span>';
+                }
+                if (value === 2) {
+                    return '<span">账户充值</span>';
+                }
+            }
+            },
+            {label: '商品值', name: 'value', index: 'value', width: 80},
+            {label: '商品价格', name: 'money', index: 'money', width: 80},
+            {label: '创建时间', name: 'createTime', index: 'create_time', width: 80}
         ],
-		viewrecords: true,
+        viewrecords: true,
         height: 385,
         rowNum: 10,
-		rowList : [10,30,50],
-        rownumbers: true, 
-        rownumWidth: 25, 
-        autowidth:true,
+        rowList: [10, 30, 50],
+        rownumbers: true,
+        rownumWidth: 25,
+        autowidth: true,
         multiselect: true,
         pager: "#jqGridPager",
-        jsonReader : {
+        jsonReader: {
             root: "page.list",
             page: "page.currPage",
             total: "page.totalPage",
             records: "page.totalCount"
         },
-        prmNames : {
-            page:"page", 
-            rows:"limit", 
+        prmNames: {
+            page: "page",
+            rows: "limit",
             order: "order"
         },
-        gridComplete:function(){
-        	//隐藏grid底部滚动条
-        	$("#jqGrid").closest(".ui-jqgrid-bdiv").css({ "overflow-x" : "hidden" }); 
+        gridComplete: function () {
+            //隐藏grid底部滚动条
+            $("#jqGrid").closest(".ui-jqgrid-bdiv").css({"overflow-x": "hidden"});
         }
     });
 });
 
 var vm = new Vue({
-	el:'#rrapp',
-	data:{
-		showList: true,
-		title: null,
-		goods: {}
-	},
-	methods: {
-		query: function () {
-			vm.reload();
-		},
-		add: function(){
-			vm.showList = false;
-			vm.title = "新增";
-			vm.goods = {};
-		},
-		update: function (event) {
-			var id = getSelectedRow();
-			if(id == null){
-				return ;
-			}
-			vm.showList = false;
+    el: '#rrapp',
+    data: {
+        showList: true,
+        title: null,
+        goods: {}
+    },
+    methods: {
+        query: function () {
+            vm.reload();
+        },
+        add: function () {
+            vm.showList = false;
+            vm.title = "新增";
+            vm.goods = {};
+        },
+        update: function (event) {
+            var id = getSelectedRow();
+            if (id == null) {
+                return;
+            }
+            vm.showList = false;
             vm.title = "修改";
-            
+
             vm.getInfo(id)
-		},
-		saveOrUpdate: function (event) {
-			var url = vm.goods.id == null ? "goods/save" : "goods/update";
-			$.ajax({
-				type: "POST",
-			    url: baseURL + url,
+        },
+        saveOrUpdate: function (event) {
+            var url = vm.goods.id == null ? "goods/save" : "goods/update";
+            $.ajax({
+                type: "POST",
+                url: baseURL + url,
                 contentType: "application/json",
-			    data: JSON.stringify(vm.goods),
-			    success: function(r){
-			    	if(r.code === 0){
-						alert('操作成功', function(index){
-							vm.reload();
-						});
-					}else{
-						alert(r.msg);
-					}
-				}
-			});
-		},
-		del: function (event) {
-			var ids = getSelectedRows();
-			if(ids == null){
-				return ;
-			}
-			
-			confirm('确定要删除选中的记录？', function(){
-				$.ajax({
-					type: "POST",
-				    url: baseURL + "goods/delete",
+                data: JSON.stringify(vm.goods),
+                success: function (r) {
+                    if (r.code === 0) {
+                        alert('操作成功', function (index) {
+                            vm.reload();
+                        });
+                    } else {
+                        alert(r.msg);
+                    }
+                }
+            });
+        },
+        del: function (event) {
+            var ids = getSelectedRows();
+            if (ids == null) {
+                return;
+            }
+
+            confirm('确定要删除选中的记录？', function () {
+                $.ajax({
+                    type: "POST",
+                    url: baseURL + "goods/delete",
                     contentType: "application/json",
-				    data: JSON.stringify(ids),
-				    success: function(r){
-						if(r.code == 0){
-							alert('操作成功', function(index){
-								$("#jqGrid").trigger("reloadGrid");
-							});
-						}else{
-							alert(r.msg);
-						}
-					}
-				});
-			});
-		},
-		getInfo: function(id){
-			$.get(baseURL + "goods/info/"+id, function(r){
+                    data: JSON.stringify(ids),
+                    success: function (r) {
+                        if (r.code == 0) {
+                            alert('操作成功', function (index) {
+                                $("#jqGrid").trigger("reloadGrid");
+                            });
+                        } else {
+                            alert(r.msg);
+                        }
+                    }
+                });
+            });
+        },
+        getInfo: function (id) {
+            $.get(baseURL + "goods/info/" + id, function (r) {
                 vm.goods = r.goods;
             });
-		},
-		reload: function (event) {
-			vm.showList = true;
-			var page = $("#jqGrid").jqGrid('getGridParam','page');
-			$("#jqGrid").jqGrid('setGridParam',{ 
-                page:page
+        },
+        reload: function (event) {
+            vm.showList = true;
+            var page = $("#jqGrid").jqGrid('getGridParam', 'page');
+            $("#jqGrid").jqGrid('setGridParam', {
+                page: page
             }).trigger("reloadGrid");
-		}
-	}
+        }
+    }
 });
