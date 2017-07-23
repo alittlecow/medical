194f44c574a249f288312cb133a197143f54a405
pengxiao <pengxiao@weshare.com.cn>
2017-07-23 10:59:25 +0800

    id卡

--- src/main/java/com/jubo/modules/sys/service/impl/CardServiceImpl.java
+++ src/main/java/com/jubo/modules/sys/service/impl/CardServiceImpl.java
@@ -1,55 +1,61 @@
 package com.jubo.modules.sys.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Map;
-
+import com.jubo.common.utils.UUIDUtil;
 import com.jubo.modules.sys.dao.CardDao;
 import com.jubo.modules.sys.entity.CardEntity;
 import com.jubo.modules.sys.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 
 @Service("cardService")
 public class CardServiceImpl implements CardService {
-	@Autowired
-	private CardDao cardDao;
-	
-	@Override
-	public CardEntity queryObject(String id){
-		return cardDao.queryObject(id);
-	}
-	
-	@Override
-	public List<CardEntity> queryList(Map<String, Object> map){
-		return cardDao.queryList(map);
-	}
-	
-	@Override
-	public int queryTotal(Map<String, Object> map){
-		return cardDao.queryTotal(map);
-	}
-	
-	@Override
-	public void save(CardEntity card){
-		cardDao.save(card);
-	}
-	
-	@Override
-	public void update(CardEntity card){
-		cardDao.update(card);
-	}
-	
-	@Override
-	public void delete(String id){
-		cardDao.delete(id);
-	}
-	
-	@Override
-	public void deleteBatch(String[] ids){
-		cardDao.deleteBatch(ids);
-	}
-	
+    @Autowired
+    private CardDao cardDao;
+
+    @Override
+    public CardEntity queryObject(String id) {
+        return cardDao.queryObject(id);
+    }
+
+    @Override
+    public List<CardEntity> queryList(Map<String, Object> map) {
+        return cardDao.queryList(map);
+    }
+
+    @Override
+    public int queryTotal(Map<String, Object> map) {
+        return cardDao.queryTotal(map);
+    }
+
+    @Override
+    public void save(CardEntity card) {
+        String id = UUIDUtil.getUUId();
+        Date nowTime = new Date();
+        card.setLastUseTime(nowTime);
+        card.setCreateTime(nowTime);
+        card.setId(id);
+        cardDao.save(card);
+    }
+
+    @Override
+    public void update(CardEntity card) {
+        card.setLastUseTime(new Date());
+        cardDao.update(card);
+    }
+
+    @Override
+    public void delete(String id) {
+        cardDao.delete(id);
+    }
+
+    @Override
+    public void deleteBatch(String[] ids) {
+        cardDao.deleteBatch(ids);
+    }
+
 }
--- src/main/resources/static/js/modules/sys/card.js
+++ src/main/resources/static/js/modules/sys/card.js
@@ -2,120 +2,126 @@ $(function () {
     $("#jqGrid").jqGrid({
         url: baseURL + 'card/list',
         datatype: "json",
-        colModel: [			
-			{ label: 'id', name: 'id', index: 'id', width: 50, key: true },
-			{ label: '用户id', name: 'userId', index: 'user_id', width: 80 }, 			
-			{ label: '设备编码', name: 'code', index: 'code', width: 80 }, 			
-			{ label: '剩余使用次数', name: 'count', index: 'count', width: 80 }, 			
-			{ label: '创建时间', name: 'createTime', index: 'create_time', width: 80 }, 			
-			{ label: '上次使用时间', name: 'lastUseTime', index: 'last_use_time', width: 80 }			
+        colModel: [
+            {label: 'id', name: 'id', index: 'id', width: 50, key: true, hidden: true},
+            {label: '用户id', name: 'userId', index: 'user_id', width: 80},
+            {label: '设备编码', name: 'code', index: 'code', width: 80},
+            {label: '剩余使用次数', name: 'count', index: 'count', width: 80},
+            {label: '创建时间', name: 'createTime', index: 'create_time', width: 80},
+            {label: '上次使用时间', name: 'lastUseTime', index: 'last_use_time', width: 80}
         ],
-		viewrecords: true,
+        viewrecords: true,
         height: 385,
         rowNum: 10,
-		rowList : [10,30,50],
-        rownumbers: true, 
-        rownumWidth: 25, 
-        autowidth:true,
+        rowList: [10, 30, 50],
+        rownumbers: true,
+        rownumWidth: 25,
+        autowidth: true,
         multiselect: true,
         pager: "#jqGridPager",
-        jsonReader : {
+        jsonReader: {
             root: "page.list",
             page: "page.currPage",
             total: "page.totalPage",
             records: "page.totalCount"
         },
-        prmNames : {
-            page:"page", 
-            rows:"limit", 
+        prmNames: {
+            page: "page",
+            rows: "limit",
             order: "order"
         },
-        gridComplete:function(){
-        	//隐藏grid底部滚动条
-        	$("#jqGrid").closest(".ui-jqgrid-bdiv").css({ "overflow-x" : "hidden" }); 
+        gridComplete: function () {
+            //隐藏grid底部滚动条
+            $("#jqGrid").closest(".ui-jqgrid-bdiv").css({"overflow-x": "hidden"});
         }
     });
 });
 
 var vm = new Vue({
-	el:'#rrapp',
-	data:{
-		showList: true,
-		title: null,
-		card: {}
-	},
-	methods: {
-		query: function () {
-			vm.reload();
-		},
-		add: function(){
-			vm.showList = false;
-			vm.title = "新增";
-			vm.card = {};
-		},
-		update: function (event) {
-			var id = getSelectedRow();
-			if(id == null){
-				return ;
-			}
-			vm.showList = false;
+    el: '#rrapp',
+    data: {
+        q: {
+            userId: null
+        },
+        showList: true,
+        title: null,
+        card: {}
+    },
+    methods: {
+        query: function () {
+            vm.reload();
+        },
+        add: function () {
+            vm.showList = false;
+            vm.title = "新增";
+            vm.card = {};
+        },
+        update: function (event) {
+            var id = getSelectedRow();
+            if (id == null) {
+                return;
+            }
+            vm.showList = false;
             vm.title = "修改";
-            
+
             vm.getInfo(id)
-		},
-		saveOrUpdate: function (event) {
-			var url = vm.card.id == null ? "card/save" : "card/update";
-			$.ajax({
-				type: "POST",
-			    url: baseURL + url,
+        },
+        saveOrUpdate: function (event) {
+            var url = vm.card.id == null ? "card/save" : "card/update";
+            $.ajax({
+                type: "POST",
+                url: baseURL + url,
                 contentType: "application/json",
-			    data: JSON.stringify(vm.card),
-			    success: function(r){
-			    	if(r.code === 0){
-						alert('操作成功', function(index){
-							vm.reload();
-						});
-					}else{
-						alert(r.msg);
-					}
-				}
-			});
-		},
-		del: function (event) {
-			var ids = getSelectedRows();
-			if(ids == null){
-				return ;
-			}
-			
-			confirm('确定要删除选中的记录？', function(){
-				$.ajax({
-					type: "POST",
-				    url: baseURL + "card/delete",
+                data: JSON.stringify(vm.card),
+                success: function (r) {
+                    if (r.code === 0) {
+                        alert('操作成功', function (index) {
+                            vm.reload();
+                        });
+                    } else {
+                        alert(r.msg);
+                    }
+                }
+            });
+        },
+        del: function (event) {
+            var ids = getSelectedRows();
+            if (ids == null) {
+                return;
+            }
+
+            confirm('确定要删除选中的记录？', function () {
+                $.ajax({
+                    type: "POST",
+                    url: baseURL + "card/delete",
                     contentType: "application/json",
-				    data: JSON.stringify(ids),
-				    success: function(r){
-						if(r.code == 0){
-							alert('操作成功', function(index){
-								$("#jqGrid").trigger("reloadGrid");
-							});
-						}else{
-							alert(r.msg);
-						}
-					}
-				});
-			});
-		},
-		getInfo: function(id){
-			$.get(baseURL + "card/info/"+id, function(r){
+                    data: JSON.stringify(ids),
+                    success: function (r) {
+                        if (r.code == 0) {
+                            alert('操作成功', function (index) {
+                                $("#jqGrid").trigger("reloadGrid");
+                            });
+                        } else {
+                            alert(r.msg);
+                        }
+                    }
+                });
+            });
+        },
+        getInfo: function (id) {
+            $.get(baseURL + "card/info/" + id, function (r) {
                 vm.card = r.card;
             });
-		},
-		reload: function (event) {
-			vm.showList = true;
-			var page = $("#jqGrid").jqGrid('getGridParam','page');
-			$("#jqGrid").jqGrid('setGridParam',{ 
-                page:page
+        },
+        reload: function (event) {
+            vm.showList = true;
+            var page = $("#jqGrid").jqGrid('getGridParam', 'page');
+            $("#jqGrid").jqGrid('setGridParam', {
+                postData: {
+                    'userId': vm.q.userId
+                },
+                page: page
             }).trigger("reloadGrid");
-		}
-	}
+        }
+    }
 });
